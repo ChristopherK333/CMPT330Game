632f58a5e69becf5000355ab16ef1b431f01db9c
zhirep <pelayoz@mymacewan.ca>
2026-01-28 21:38:45 -0700

    added shooting feature

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Trail.SetActive(false);
         rb = GetComponent<Rigidbody2D>();
     }
 
