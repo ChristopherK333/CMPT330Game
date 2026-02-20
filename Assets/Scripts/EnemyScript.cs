b6d2e051b47b74623d5d488d0e6a039a93b52ce0
zhirep <164239262+zhirep@users.noreply.github.com>
2026-02-19 19:22:52 -0700

    simple ai

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public Transform player;
+    public float speed = 3f;
+    public float chaseDistance = 5f;
+
     public float damage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (player == null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+            {
+                player = playerObject.transform;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+            if (distanceToPlayer < chaseDistance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+                // transform.LookAt(player.position);  // rotation
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
